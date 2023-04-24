444c749bfc1aa5346358e8a47ad22f78c570d18f
nkwaaaa <nkwaaaa@gmail.com>
2023-04-24 11:56:09 -0300

    add projects taks graphql

--- server/graphql/resolvers.js
+++ server/graphql/resolvers.js
@@ -1,8 +1,17 @@
 import Project from "./models/Project.js";
+import Task from "./models/Task.js";
 
 export const resolvers = {
 	Query: {
 		hello: () => "Hello World",
+
+		projects: async () => {
+			return await Project.find();
+		},
+
+		tasks: async () => {
+			return await Task.find();
+		},
 	},
 	Mutation: {
 		createProject: async (_, { name, description }) => {
@@ -12,6 +21,19 @@ export const resolvers = {
 			});
 			const savedProject = project.save();
 			return savedProject;
+		},
+		createTask: async (_, { title, projectId }) => {
+			const projectFound = await Project.findById(projectId);
+			if (!projectFound) {
+				throw new Error("Project not found F");
+			}
+
+			const task = new Task({
+				title,
+				projectId,
+			});
+			const savedTask = task.save();
+			return savedTask;
 		},
 	},
 };
