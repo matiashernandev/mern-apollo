83dffdbe2f3d9578ea9a7c261d829da09c5f16b9
nkwaaaa <nkwaaaa@gmail.com>
2023-04-24 17:53:16 -0300

    add create project

--- client/src/graphql/projects.js
+++ client/src/graphql/projects.js
@@ -9,3 +9,13 @@ export const GET_PROJECTS = gql`
 		}
 	}
 `;
+
+export const CREATE_PROJECT = gql`
+	mutation CreateProject($name: String!, $description: String!) {
+		createProject(name: $name, description: $description) {
+			_id
+			name
+			description
+		}
+	}
+`;
